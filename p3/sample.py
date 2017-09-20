6dc479591b9c7bd9876923f818f66f999b51d898
Robert <Robert@25.12.132.115>
2017-09-20 13:39:10 -0500

    Almost done with format for easily inputting algorithms.

--- p3/sample.py
+++ p3/sample.py
@@ -1,3 +1,4 @@
+import p3.actions
 '''
 Created on Sep 7, 2017
 
@@ -9,13 +10,14 @@ class sample(object):
     classdocs
     '''
     
-
-    def __init__(self, last_thirty_actions, last_actions_were_success, next_sample_loc, next_sample_loc_num):
+    def __init__(self):
         '''
         Constructor
         '''
-        self.last_thirty_actions = last_thirty_actions
-        self.last_actions_were_success = last_actions_were_success
-        self.next_sample_loc = next_sample_loc
-        self.next_sample_loc_num = next_sample_loc_num
+        function_names = [func for func in dir(p3.actions.Actions) if callable(getattr(p3.actions.Actions, func)) and not func.startswith('_')]
+        self.rewards = []
+        for _ in function_names:
+            self.rewards.append(0)
             
+    def set_reward(self, action_num, reward):
+        self.rewards[action_num] = reward
